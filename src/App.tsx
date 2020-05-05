ef2aab2beccf0d744fa16914fb287a1219d98fff
Huasheng <huashengwang1989@gmail.com>
2020-05-06 00:57:36 +0800

    Updated data for 2020/05/05

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ const App = () => {
 
   const legendDisplayThreshold = 1112 * 834 // iPad Pro 10.5
 
-  const today = '2020-05-03'
+  const today = '2020-05-05'
 
   const [chartScale, setChartScale] = useState(
     (localStorage.getItem('sgcovid.chart.scale') || 'linear') as
