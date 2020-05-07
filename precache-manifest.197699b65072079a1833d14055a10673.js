4d051c4026a9603f2caf30e8cc873e7f930194db
Huasheng <huashengwang1989@gmail.com>
2020-05-07 23:11:40 +0800

    Deploy commit

---
+++ precache-manifest.197699b65072079a1833d14055a10673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a382b2158956579a78972bd007aa5b99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e03aec2c28f129beb089",
+    "url": "./static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "d91d9c9760d2c292599b",
+    "url": "./static/css/main.84bce44e.chunk.css"
+  },
+  {
+    "revision": "e03aec2c28f129beb089",
+    "url": "./static/js/2.b63dd046.chunk.js"
+  },
+  {
+    "revision": "c3a7259939951a6ccc4dc35d61f33f61",
+    "url": "./static/js/2.b63dd046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91d9c9760d2c292599b",
+    "url": "./static/js/main.d6fb744f.chunk.js"
+  },
+  {
+    "revision": "389f4a129a7629ed53d1",
+    "url": "./static/js/runtime-main.9814c380.js"
+  }
+]);
